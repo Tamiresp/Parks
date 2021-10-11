3e564a89e92a5b4aa21ae1a87d2d6143c0a9b7c9
Tamires Pereira <tps@cesar.org.br>
2021-10-11 16:58:52 -0300

    events

--- lib/list/events/event_item.dart
+++ lib/list/events/event_item.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:parks/data/events_data.dart';
 
 class EventsItemPage extends StatefulWidget {
-  const EventsItemPage({Key? key, this.title, required this.event}) : super(key: key);
+  const EventsItemPage({Key? key, this.title, required this.event})
+      : super(key: key);
   final String? title;
   final Events event;
 
@@ -18,13 +19,12 @@ class EventItemPageState extends State<EventsItemPage> {
   Widget build(BuildContext context) {
     return Container(
         height: 50,
-        color: Colors.white,
         margin: EdgeInsets.only(left: 16, right: 16, top: 8),
         child: Align(
           alignment: Alignment.centerLeft,
           child: Text(
             event.name,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
           ),
         ));
   }
